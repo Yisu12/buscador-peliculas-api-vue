5ec42615ccbd9a354ea94ebaf40bb61a5c747aa2
Yisu12 <81430614+Yisu12@users.noreply.github.com>
2022-02-16 23:03:23 +0000

    v1 Buscador peliculas
    Aplicación finalizada y desplegada.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            title: "API Películas Vue",
+            movieData: {},
+            movieTitle: "Spider Man",
+        }
+    },
+    mounted(){
+        this.getMovie();
+    },
+    methods: {
+        async getMovie() {
+            const search = this.movieTitle.toLowerCase().replace(/ /g, "+");
+            
+            const data = await fetch(`https://www.omdbapi.com/?apikey=91f16f5d&t=${search}`);
+
+            const jsonData = await data.json();
+            console.log(jsonData);
+            this.movieData = jsonData;
+        }
+    }
+})
